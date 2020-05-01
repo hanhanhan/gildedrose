ad279807d1ca57caeb65a6eb777abd684e6ed514
Hannah Lazarus <hanhanhan.lazarus@gmail.com>
2020-05-01 16:35:46 -0400

    procedural refactor

--- gildedrose.py
+++ gildedrose.py
@@ -1,37 +1,42 @@
-class GildedRose(object):
+class GildedRose:
 
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
+
+            item.sell_in = item.sell_in - 1
+
+            if item.name == "Aged Brie":
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+                if item.quality < 50 and item.sell_in < 0:
+                    item.quality = item.quality + 1
+                continue
+
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+
+                if item.sell_in < 0:
+                    item.quality = 0
+                elif item.sell_in < 5:
+                    item.quality = item.quality + 3
+                elif item.sell_in < 10:
+                    item.quality = item.quality + 2
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    item.quality = item.quality + 1
+
+                item.quality = min(item.quality, 50)
+                continue
+
+            if item.quality > 0:
+                item.quality = item.quality - 1
+
+            if item.sell_in < 0:
+                if item.quality > 0:
+                    item.quality = item.quality - 1
 
 
 class Item:
@@ -40,5 +45,5 @@ class Item:
         self.sell_in = sell_in
         self.quality = quality
 
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+    def __str__(self):
+        return f"{self.name}, Sell in: {self.sell_in}, Quality: {self.quality}"
